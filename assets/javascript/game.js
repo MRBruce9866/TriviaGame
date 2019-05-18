25a3766f8fc879d6c7dfc696d2114672cd43134e
Mark <mrbruce9866@gmail.com>
2019-05-18 02:56:44 -0400

    implemented the start button and timers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
 
-    var temp = $('.gameQuestion');
-    game.shuffleQuestions();
-    console.log(game.questionBank);
-
+    $('.startBtn').on("click", function () {
+        game.startGame();
+    });
 
 });
 
@@ -11,38 +10,32 @@ $(document).ready(function () {
 var game = {
     totalQuestions: 10,
     rightAnswers: 0,
-    timePerQuestion: 20, //time in seconds
-    timeLeft: this.timePerQuestion,
+    timePerQuestion: 2, //time in seconds
+    timeLeft: 2,
     currentQuestionIndex: 0,
     questionIndex: 0,
     timerIntervalID: 0,
     correctAnswer: false,
+    isRunning: false,
 
     questionBank: [ //the correct answer is stored in the index 0 position of the answers array.
-        new Question("", ["", "", "", ""], "", "")
-        // new Question("B", ["", "", "", ""]),
-        // new Question("C", ["", "", "", ""]),
-        // new Question("D", ["", "", "", ""]),
-        // new Question("E", ["", "", "", ""]),
-        // new Question("F", ["", "", "", ""]),
-        // new Question("G", ["", "", "", ""]),
-        // new Question("H", ["", "", "", ""]),
-        // new Question("I", ["", "", "", ""]),
-        // new Question("J", ["", "", "", ""]),
-        // new Question("K", ["", "", "", ""]),
-        // new Question("L", ["", "", "", ""]),
-        // new Question("M", ["", "", "", ""]),
-        // new Question("N", ["", "", "", ""]),
-        // new Question("O", ["", "", "", ""]),
-        // new Question("P", ["", "", "", ""]),
-        // new Question("Q", ["", "", "", ""]),
-        // new Question("R", ["", "", "", ""]),
-        // new Question("S", ["", "", "", ""]),
-        // new Question("T", ["", "", "", ""]),
+        new Question("What color do the ghost enemies turn when Pacman eats a power pellet?", ["Blue", "Red", "Yellow", "Green"], ""),
+        new Question("How long did it take Markus Persson to create the first version of Minecraft?", ["1 week", "3 days", "1 month", "2 weeks"], ""),
+        new Question("In every Mario Kart title, what is the name of the last track of the Special Cup?", ["Rainbow Road", "Bowser Castle", "Mario Circuit", "Bowser's Keep"], ""),
+        new Question("Crash Bandicoot was released in what year?", ["1996", "1994", "1999", "1998"], ""),
+        new Question("Which Mario character has appeared in all of the Mario Party games EXCEPT one?", ["Wario", "Waluigi", "Bowser", "Peach"], ""),
+        new Question("Solid Snake is a hero in what video game series?", ["Metal Gear", "Red Dead Redemption", "Castlevania", "Fallout"], ""),
+        // new Question("", ["", "", "", ""],""),
+        // new Question("", ["", "", "", ""],""),
+        // new Question("", ["", "", "", ""],""),
+
     ],
 
 
     startGame: function () {
+        $('.startBtn').hide();
+        this.shuffleQuestions();
+        this.displayQuestion(this.questionBank[this.questionIndex]);
 
     },
 
@@ -59,17 +52,18 @@ var game = {
 
         if (this.questionIndex >= this.totalQuestions - 1) {
             this.displayResults();
-        }else{
+        } else {
+            console.log(this)
             this.displayQuestion(this.questionBank[this.questionIndex]);
         }
 
     },
 
-    checkAnswer: function(answer){
+    checkAnswer: function (answer) {
 
-        if(answer === this.questionBank[this.questionIndex].answers[0]){
+        if (answer === this.questionBank[this.questionIndex].answers[0]) {
             this.correctAnswer = true;
-        }else{
+        } else {
             this.correctAnswer = false;
         }
 
@@ -77,19 +71,31 @@ var game = {
 
     },
 
-    displayQuestion: function (question) {
+    displayQuestion: function (quest) {
+        //show Timer Display
+        $('.gameTimer').append($('<div>').addClass('timerDisplay mx-auto').text(this.timeLeft));
 
+        //show question
+        $('.gameQuestion').text(quest.question);
+
+        $('.gameAnswerBox').show();
+
+        for (let i = 0; i < quest.answers.length; i++) {
+            $('#gameAnswer' + i).text(this.questionBank[this.questionIndex].answers[i]);
+        }
+        this.timeLeft = this.timePerQuestion;
+        this.startTimer();
 
     },
 
     displayAnswer: function () {
-        if(correctAnswer){
+        if (this.correctAnswer) {
 
-        }else{
+        } else {
 
         }
 
-        setTimeout(nextQuestion, 5000);
+        setTimeout(this.nextQuestion, 5000);
     },
 
     displayResults: function () {
@@ -117,23 +123,41 @@ var game = {
     },
 
     startTimer: function () {
-        this.timerIntervalID = setInterval(countDown, 1000);
+        console.log(this)
+
+        var obj = this;
+
+        if (!this.isRunning) {
+            this.isRunning = true;
+            this.timerIntervalID = setInterval(function(){
+                obj.countDown(obj);
+            }, 1000);
+        }
     },
 
     clearTimer: function () {
+        this.isRunning = false;
         clearInterval(this.timerIntervalID);
     },
 
-    countDown: function () {
-        this.timeLeft--;
-        if (this.timeLeft <= 0) this.timesUp();
+    countDown: function (obj) {
+        console.log(this);
+        obj.timeLeft--;
+        console.log(obj.timeLeft);
+        if (obj.timeLeft <= 0) {
+            obj.timesUp();
+        } else {
+            $('.timerDisplay').text(obj.timeLeft);
+        }
+
     },
 
     timesUp: function () {
+        this.clearTimer();
 
         this.displayTimesUp();
 
-        setTimeout(nextQuestion, 5000);
+        setTimeout(this.nextQuestion, 5000);
 
     }
 
@@ -152,5 +176,4 @@ function Question(quest, answerArray, img, desc) {
     this.question = quest;
     this.answers = answerArray;
     this.image = img;
-    this.fact = desc;
 }
